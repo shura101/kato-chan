f058cf3ae1422ea548401a6e97bb7c8242691d6e
elxxena <andrekusuma388@gmail.com>
2025-04-14 08:46:29 +0700

    update waifu.py

--- utils/waifu.py
+++ utils/waifu.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_waifu_image(nsfw=False):
+def get_waifu_image(nsfw=True):
     endpoint = "https://api.waifu.pics/nsfw/waifu" if nsfw else "https://api.waifu.pics/sfw/waifu"
     response = requests.get(endpoint)
     if response.status_code == 200:
