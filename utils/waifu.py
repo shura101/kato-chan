56fe2919568fbfcd54394c9a6a46885a302a0d82
Shura <59175804+shura101@users.noreply.github.com>
2025-04-14 22:40:01 +0700

    update

--- utils/waifu.py
+++ utils/waifu.py
@@ -1,4 +1,15 @@
 import requests
+import aiohttp
+
+async def fetch_random_waifu():
+    url = "https://api.waifu.pics/sfw/waifu"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data["url"]
+            else:
+                return "https://media.tenor.com/f0uvs96FfXsAAAAd/waifu-anime.gif"  # fallback image
 
 def get_waifu_image(nsfw=True):
     endpoint = "https://api.waifu.pics/nsfw/waifu" if nsfw else "https://api.waifu.pics/sfw/waifu"
