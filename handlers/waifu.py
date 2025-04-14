a98fb51f27368c4b77860db96b2562fd296b52cd
Shura <59175804+shura101@users.noreply.github.com>
2025-04-14 22:36:33 +0700

    update

--- handlers/waifu.py
+++ handlers/waifu.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from utils.waifu import get_waifu_image  # Mengimpor fungsi get_waifu_image
+import aiohttp
 
 async def waifu_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Periksa apakah mode NSFW diminta
@@ -15,3 +16,13 @@ async def waifu_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_photo(photo=url, caption=f"Mode: {mode.upper()} | Waifu")
     else:
         await update.message.reply_text("Gagal mengambil gambar waifu.")
+
+async def fetch_random_waifu():
+    url = "https://api.waifu.pics/sfw/waifu"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data["url"]
+            else:
+                return "https://media.tenor.com/f0uvs96FfXsAAAAd/waifu-anime.gif"  # fallback
